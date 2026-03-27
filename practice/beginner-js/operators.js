0dcb51431db429f69cafaa1b6e05ee92712283d8
DhanushKeloth <dhanushkeloth@gmail.com>
2026-03-27 15:14:29 +0530

    done with the operators in javascript and type coercion

---
+++ practice/beginner-js/operators.js
@@ -0,0 +1,40 @@
+//arithmetic operators
+a=10
+b=2
+console.log("arithmetic operators")
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+console.log("\n")
+
+//comparison operators
+console.log("comparison operators")
+console.log(a==b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log("\n")
+
+//strict equality 
+console.log("strict comparison operators")
+num1=100
+num2="100"
+console.log(num1===num2)
+console.log(num1!==num2)
+console.log("\n")
+
+//logical operators
+console.log("logical operators")
+console.log(a>b && a<b)
+console.log(a<b || a>b)
+console.log(!a)
+
+//type coercion 
+console.log(10+"20") ///string concatenation
+console.log(10-"5") ///number substraction
+console.log(10*"5") ///number multiplication
