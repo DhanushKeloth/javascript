70119a9566145760f12243a9aebd5b0df28db3a2
DhanushKeloth <dhanushkeloth@gmail.com>
2026-03-30 20:09:10 +0530

    integrated edit task feature and task update feature using checkbox

--- tasks/task-1/todo.js
+++ tasks/task-1/todo.js
@@ -2,35 +2,89 @@ const textinput = document.getElementById("task-input");
 const addBtn = document.getElementById("add-btn");
 const taskList = document.getElementById("task-list");
 
-const tasks = [];
-const storedtasks= localStorage.getItem("tasks");
-if(storedtasks){
-    tasks.push(...JSON.parse(storedtasks));
-    renderTasks();
-}
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+renderTasks();  
 function renderTasks(){
     taskList.innerHTML = "";
     tasks.forEach((task,index)=>{
+        console.log(task,index)
         const li = document.createElement("li");
-        li.textContent=task;
+
+        
+
+        //checkbox
+        const checkbox = document.createElement("input");
+        checkbox.type="checkbox";
+        checkbox.checked=task.completed;
+
+        checkbox.addEventListener("change",()=>{
+            tasks[index].completed=checkbox.checked;
+            
+            localStorage.setItem("tasks",JSON.stringify(tasks));
+             renderTasks();
+        })
+
+        const span = document.createElement("span");
+        span.textContent=task.text;
+        span.classList.add("task-text");
+        if(task.completed){
+            span.style.textDecoration="line-through";
+        }
+        span.appendChild(checkbox);
+        li.appendChild(span);
+        //btn container
+        const btncontainer = document.createElement("div");
+        btncontainer.classList.add("btn-container");
+        
+
+
+        const editbtn = document.createElement("button");
+        editbtn.textContent="Edit";
+        editbtn.classList.add("edit-btn");
+
+        editbtn.addEventListener("click",(e)=>{
+            span.contentEditable=true;
+            span.focus();
+            const newtext = prompt("Edit task:",task.text);
+            if(newtext){
+                tasks[index].text=newtext;
+                localStorage.setItem("tasks",JSON.stringify(tasks));
+                renderTasks();
+            }
+
+        })
+        
+
         const delbtn = document.createElement("button");
         delbtn.textContent="Delete";
         delbtn.classList.add("delete-btn");
+
         delbtn.addEventListener("click",()=>{
             console.log("delete clicked")
             tasks.splice(index,1);
             localStorage.setItem("tasks",JSON.stringify(tasks));
             renderTasks();
         })
-        li.appendChild(delbtn);
+
+
+        // li.appendChild(editbtn);
+        // li.appendChild(delbtn);
+        li.appendChild(btncontainer);
+        
+        btncontainer.appendChild(editbtn);
+        btncontainer.appendChild(delbtn);
         taskList.appendChild(li);
     })
 }
 addBtn.addEventListener("click",()=>{
     const task = textinput.value.trim();
     if(task){
-        tasks.push(task);   
+        tasks.push({
+            text:task,
+            completed:false
+        });   
         localStorage.setItem("tasks",JSON.stringify(tasks));
+        console.log(tasks);
         textinput.value="";
         renderTasks();
     }
