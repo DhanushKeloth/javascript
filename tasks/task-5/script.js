6fe487249a090293efc99e5bc8594cc9b1fdf5ea
DhanushKeloth <dhanushkeloth@gmail.com>
2026-04-04 22:53:29 +0530

    created the cart logic with dynamic item price calculation based on product item quantity

--- tasks/task-5/script.js
+++ tasks/task-5/script.js
@@ -1,6 +1,12 @@
 import { data } from "./exportData.js";
 
+//store the product data in global variable
+let productsData =[];
+
 data.then((product)=>{
+    //store the data
+    productsData=product;
+
     const maincontainer = document.querySelector(".main");
     
     const productContainer = document.createElement("div")
@@ -26,7 +32,8 @@ data.then((product)=>{
         const button = document.createElement("button");
         button.textContent="Add to Cart";
         button.classList.add("addToCart");
-        button.dataset.id=item.id;
+        button.dataset.id=item.id; //store the id of the item in the dataset
+        
         card.appendChild(button);
 
         productContainer.appendChild(card);
@@ -34,8 +41,84 @@ data.then((product)=>{
     maincontainer.appendChild(productContainer);
     
 })
+
 document.querySelector(".main").addEventListener("click", (e) => {
   if (e.target.classList.contains("addToCart")) {
-    console.log(e.target.dataset.id);
+    const id=e.target.dataset.id;
+    //can also be done using map method 
+    // const productMap = new Map();
+    // productsData.forEach((item)=>{
+      //   productMap.set(item.id,item);
+      // })
+
+    
+    
+
+    const product = productsData.find((item)=>item.id==id);
+    console.log(product)
+    
+    const cart  = document.querySelector(".cart");
+    
+    const cartItems = document.createElement("div");
+    cartItems.classList.add("cart-items");
+
+    const imageItem = document.createElement("img");
+    imageItem.src='./assets/'+product.image.slice(9);
+
+    const nameItem = document.createElement("h3");
+    nameItem.textContent=product.name;
+
+    const priceItem = document.createElement("span");
+    priceItem.textContent=product.price;
+
+    const cartButtonGroup = document.createElement("div");
+    cartButtonGroup.classList.add("buttons-group");
+
+    const decreaseButton = document.createElement("button")
+    decreaseButton.textContent="-";
+
+    const cartItemQuantity = document.createElement("span");
+    cartItemQuantity.textContent=1;
+
+    const increaseButton = document.createElement("button")
+    increaseButton.textContent="+";
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent="Remove";
+    removeButton.setAttribute("id","remove-btn");
+
+    removeButton.addEventListener("click",(e)=>{
+      cartItems.remove();
+    })
+
+    cartItems.appendChild(imageItem);
+    cartItems.appendChild(nameItem);
+    cartItems.appendChild(priceItem);
+    cartItems.appendChild(cartButtonGroup);
+    cartButtonGroup.appendChild(decreaseButton);
+    cartButtonGroup.appendChild(cartItemQuantity);
+    cartButtonGroup.appendChild(increaseButton);
+    cartItems.appendChild(removeButton);
+    cart.appendChild(cartItems);
+
+    //increase quantity 
+    increaseButton.addEventListener("click",()=>{
+      cartItemQuantity.textContent++;
+      let finalprice=product.price*cartItemQuantity.textContent;
+      priceItem.textContent=Number(finalprice.toFixed(2));
+    })
+    //decrease quantity
+    decreaseButton.addEventListener("click",(e)=>{
+      if(cartItemQuantity.textContent>1){
+        cartItemQuantity.textContent--;
+        let finalprice = product.price*cartItemQuantity.textContent;
+        priceItem.textContent=Number(finalprice.toFixed(2))
+      }
+    })
+  
+    //add the cart items to the loca storage
+    // let cartItemsData = JSON.parse(localStorage.getItem("cartItems")) || [];
+    // cartItemsData.push(product.id);
+    // localStorage.setItem("cartItems",JSON.stringify(cartItemsData));
   } 
 });
